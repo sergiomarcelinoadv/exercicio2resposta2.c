02c07e2045455fdc7a8bcbd6bf5b00c7d03bce48
sergiomarcelinoadv <64876386+sergiomarcelinoadv@users.noreply.github.com>
2020-09-28 22:14:53 -0300

    Exercicio2resposta2.c

---
+++ Exercicio2resposta2.c
@@ -0,0 +1,21 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void)
+{
+	
+int num;
+printf("digite um numero inteiro: ");
+scanf("%d",&num);
+
+if(num%2==0)//operador %
+{
+	
+printf("Par");
+}
+
+else
+printf("impar");
+}
